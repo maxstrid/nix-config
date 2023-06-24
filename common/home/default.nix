4a99cd317ba1ed16a8ed9114f8b295ccdcb9d268
Maxwell Henderson <maxwell.henderson@mailbox.org>
2023-06-23 19:58:04 -0700

    Re-order home manager setup
    Signed-off-by: Maxwell Henderson <maxwell.henderson@mailbox.org>

---
+++ common/home/default.nix
@@ -0,0 +1,45 @@
+{ config, pkgs, nix-colors, ... }:
+
+{
+  imports = [
+    nix-colors.homeManagerModules.default
+    ./editor.nix
+  ];
+
+  # Based on morhetz gruvbox
+  colorScheme = {
+    slug = "gruvbox";
+    name = "Gruvbox";
+    author = "Morhetz (https://github.com/morhetz/gruvbox)";
+    colors = {
+      base00 = "#1D2021"; # BG
+      base01 = "#282828"; # Lighter BG
+      base02 = "504945"; # --
+      base03 = "665c54"; # -
+      base04 = "bdae93"; # +
+      base05 = "d5c4a1"; # ++
+      base06 = "ebdbb2"; # +++
+      base07 = "fbf1c7"; # ++++
+      base08 = "#CC241D"; # Red
+      base09 = "#D65D0E"; # Orange
+      base0A = "#D79921"; # Yellow
+      base0B = "#98971A"; # Green
+      base0C = "#689D6A"; # Aqua
+      base0D = "#458588"; # Blue
+      base0E = "#B16286"; # Purple
+      base0F = "#D65D0E"; # Brown
+    };
+  };
+
+  home.username = "max";
+  home.homeDirectory = "/home/max";
+
+  fonts.fontconfig.enable = true;
+  home.stateVersion = "23.05";
+
+  programs.home-manager.enable = true;
+  services.home-manager.autoUpgrade = {
+    enable = true;
+    frequency = "daily";
+  };
+}
