c62a4040bd75b869e48f83703e986d8162ef1cf8
Maxwell Henderson <maxwell.henderson@mailbox.org>
2023-06-25 09:45:03 -0700

    Maybe fix stuff
    Signed-off-by: Maxwell Henderson <maxwell.henderson@mailbox.org>

--- flake.nix
+++ flake.nix
@@ -20,94 +20,87 @@
     nixgl.url = "github:guibou/nixGL";
   };
 
-  outputs = { self,
-    nixpkgs,
-    home-manager,
-    rust-overlay,
-    nixos-hardware,
-    nix-colors,
-    nixgl,
-    nur,
-  ... }:
-  let
-    pkgs = import nixpkgs {
-      system = "x86_64-linux";
-      config.allowUnfree = true;
-      config.allowUnfreePredicate = (_: true);
-      overlays = [
-        rust-overlay.overlays.default
-        nur.overlay
-      ];
-    };
-  in {
-    # For my nix-on-arch setup
-    homeConfigurations = {
-      "max" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-
-        modules = [
-          nur.hmModules.nur
-          ./home
-          ./common/home
-          ./hosts/arch
-        ];
-
-        extraSpecialArgs = { inherit nix-colors nixgl; };
-      };
-    };
-    nixosConfigurations = {
-      t480 = nixpkgs.lib.nixosSystem {
-        inherit pkgs;
-
+  outputs =
+    { self
+    , nixpkgs
+    , home-manager
+    , rust-overlay
+    , nixos-hardware
+    , nix-colors
+    , nixgl
+    , nur
+    , ...
+    }:
+    let
+      pkgs = import nixpkgs {
         system = "x86_64-linux";
-        specialArgs = { inherit nix-colors; };
-        modules = [
-          nur.nixosModules.nur
-          ./hosts/t480
-          ({ pkgs, ... }: {
-            environment.systemPackages = [ pkgs.rust-bin.stable.latest.default ];
-          })
-          home-manager.nixosModules.home-manager {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.max = import ./common/home;
-          }
+        config.allowUnfree = true;
+        config.allowUnfreePredicate = (_: true);
+        overlays = [
+          rust-overlay.overlays.default
+          nur.overlay
         ];
       };
-      x220 = nixpkgs.lib.nixosSystem {
-        inherit pkgs;
+    in
+    {
+      # For my nix-on-arch setup
+      homeConfigurations = {
+        "max" = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
 
-        system = "x86_64-linux";
-        specialArgs = { inherit nix-colors; };
-        modules = [
-          nixos-hardware.nixosModules.lenovo-thinkpad-x220
-          nur.nixosModules.nur
-          ./hosts/x220
-          ({ pkgs, ... }: {
-            environment.systemPackages = [ pkgs.rust-bin.stable.latest.default ];
-          })
-          home-manager.nixosModules.home-manager {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = { inherit nix-colors; };
-            home-manager.users.max = {
-              imports = [
-                ./common/home
-                ./modules/dwm.nix
-              ];
-            };
-          }
-        ];
+          modules = [
+            nur.hmModules.nur
+            ./home
+            ./common/home
+            ./hosts/arch
+          ];
+
+          extraSpecialArgs = { inherit nix-colors nixgl; };
+        };
       };
-      home-server = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit nix-colors; };
-        modules = [
-          nixos-hardware.nixosModules.common-cpu-intel
-          nur.nixosModules.nur
-          ./hosts/home-server
-        ];
+      nixosConfigurations = {
+        t480 = nixpkgs.lib.nixosSystem {
+          inherit pkgs;
+
+          system = "x86_64-linux";
+          specialArgs = { inherit nix-colors; };
+          modules = [
+            nur.nixosModules.nur
+            ./hosts/t480
+            ({ pkgs, ... }: {
+              environment.systemPackages = [ pkgs.rust-bin.stable.latest.default ];
+            })
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.max = import ./common/home;
+            }
+          ];
+        };
+        x220 = nixpkgs.lib.nixosSystem {
+          inherit pkgs;
+          modules = [
+            home-manager.nixosModules.home-manager
+            { home-manager.users.max = ./common/home; }
+            nixos-hardware.nixosModules.lenovo-thinkpad-x220
+            nur.nixosModules.nur
+            ./hosts/x220
+            ./modules/dwm.nix
+          ];
+          specialArgs = {
+            inherit home-manager;
+          };
+        };
+        home-server = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit nix-colors; };
+          modules = [
+            nixos-hardware.nixosModules.common-cpu-intel
+            nur.nixosModules.nur
+            ./hosts/home-server
+          ];
+        };
       };
     };
-  };
 }
