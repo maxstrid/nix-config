70f6f6fce34092c57cf56115ff9af154eb67cbb2
Maxwell Henderson <maxwell.henderson@mailbox.org>
2023-06-29 08:30:44 -0700

    Fix semantic error
    Signed-off-by: Maxwell Henderson <maxwell.henderson@mailbox.org>

--- flake.nix
+++ flake.nix
@@ -110,7 +110,7 @@
               sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
               sops.age.keyFile = "/var/lib/sops-nix/key.txt";
               sops.age.generateKey = true;
-              sops.secrets.home_server.nextcloud = {
+              sops.secrets.home_server.nextcloud_password = {
                 sopsFile = ./secrets/secrets.yaml;
               };
             })
