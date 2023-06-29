021028b89252cdd85555b1ccff0129637dbacf18
Maxwell Henderson <maxwell.henderson@mailbox.org>
2023-06-29 08:44:15 -0700

    Reconfig sops
    Signed-off-by: Maxwell Henderson <maxwell.henderson@mailbox.org>

--- hosts/home-server/sops.nix
+++ hosts/home-server/sops.nix
@@ -5,7 +5,4 @@
   sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
   sops.age.keyFile = "/var/lib/sops-nix/key.txt";
   sops.age.generateKey = true;
-  sops.secrets.nextcloud_password = {
-    sopsFile = ./secrets.yaml;
-  };
 }
